215ead5f96546201332787019e044b880a44a064
Pascal Dai <daiyi@westlake.edu.cn>
2023-10-07 17:47:16 +0800

    confirm simulator complied

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>这里是应用的主页</Text>
+      <Text>点击主页中心的按钮，将会跳转到内容编辑页面</Text>
+      <Text>手势向上滑动，可以看到历史记录</Text>
+      <Text>手势向下滑动，可以进行搜索</Text>
+      <Text>如果说是移动端，将通过陀螺仪让中心的按钮进行追踪，如果是网页端，则追踪鼠标</Text>
       <StatusBar style="auto" />
     </View>
   );
